9f2db54401604489e8ab5c36d3c881d82a49803e
HarivanshL <112008912+HarivanshL@users.noreply.github.com>
2023-11-23 13:29:30 -0600

    login dynamic data fixed

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 
 const express = require('express');
+
 const app = express();
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
+
 let users = [];
 let currentUser = "";
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 /*
@@ -94,6 +97,7 @@ app.get("/signup", (req, res) => {
 })
 
 app.post("/signup", (req, res) => {
+  //CHANGE this to QUERY the database to put info
   const email = req.body.name
   const password = req.body.password
   users.push(email)
@@ -102,6 +106,8 @@ app.post("/signup", (req, res) => {
 
 })
 app.post("/login", (req, res) => {
+
+  //CHANGE this to QUERY the database for the info
   const input = req.body.email
   if(users.includes(input)){
     console.log(currentUser)
@@ -110,16 +116,23 @@ app.post("/login", (req, res) => {
     res.sendFile(path.join(__dirname, '/projects/restaurants.html'))
   }
   else{
-    res.sendFile(path.join(__dirname, '/projects/login.html'))
+    //res.sendFile(path.join(__dirname, '/projects/login.html'))
+    res.render("login", {status : "Incorrect Credentials"})
+    //res.send(fun)
+
+    //res.status(400).send("USER NOT FOUND")
+
+    //window.onload()
   }
   });
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, '/projects/login.html'))
+  //res.sendFile(path.join(__dirname, '/projects/login.html'))
+  res.render("login", {status : ""})
     //res.send("<html> <head>server Response</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>");
 });
 
 app.post("/restaurants", (req, res) => {
-
+  //ADD QUERY for posting a review 
 })
 app.get("/restaurants", (req, res) => {
   res.sendFile(path.join(__dirname, '/projects/restaurants.html'))
@@ -162,3 +175,7 @@ console.log(second)
 })
 </script>
 */
+
+function fun() {  
+  alert(location.hostname);  
+} 
